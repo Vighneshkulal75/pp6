54a4a9791a2052587ee089e8715c17d7aa6f25f6
Vigneshaa <vighneshhombadi@gmail.com>
2025-06-07 22:58:02 +0530

    fgkmmm

---
+++ src/main/java/org/example/P31.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class P31 {
+    public static void main(String[] args) {
+        System.out.println("hiiiiiiiii");
+        System.out.println("vijjjjjjjjjjj");
+    }
+}
